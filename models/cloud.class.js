ca495e456028708a398c5fa4d5586b0ec461ef7a
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-27 15:43:53 +0200

    add clouds modals / classes

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,7 +1,11 @@
 class Cloud extends movableObject{
+    y = 20
+    height = 250;
+    
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
-        this.x = 200 + Math.random() * 500;
+        this.x = Math.random() * 500;
+        this.width = 500;
     }
 }
