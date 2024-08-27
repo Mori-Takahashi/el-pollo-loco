5c113074755eab409dc35e93c25b96234c8e6462
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-27 14:14:06 +0200

    add JS modals / classes

---
+++ models/movable-object.class.js
@@ -0,0 +1,13 @@
+class movableObject {
+    x = 120;
+    y = 400;
+    img;
+
+    movingRight() {
+        console.log('movingRight');
+    }
+
+    moveLeft() {
+        console.log('moveLeft');
+    }
+}
---
+++ script/game.js
@@ -0,0 +1,10 @@
+let canvas;
+let ctx;
+let world = new World();
+
+function init() {
+    canvas = document.getElementById('canvasField');
+    ctx = canvas.getContext('2d');
+    
+    console.log('My Character is', world.character);
+}
