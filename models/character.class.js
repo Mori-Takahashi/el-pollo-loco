8aff655d0629c3a872ffe6bc8397d4c69373ae16
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-27 17:23:40 +0200

    fix with from chicken

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends movableObject{
 
+    height = 280;
+    y = 155;
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,6 +1,7 @@
 class Chicken extends movableObject{
-    y = 295;
-
+    y = 360;
+    height = 60;
+    width = 50;
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
