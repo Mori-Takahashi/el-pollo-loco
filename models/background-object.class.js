e7c56f6ae6f99c9734aca366f67caf8ac66ec2b7
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-27 16:41:32 +0200

    add background and background-object.class.js

---
+++ models/background-object.class.js
@@ -0,0 +1,10 @@
+class BackgroundObject extends movableObject{
+    
+    width = 720;
+    height = 400;
+    constructor(imagePath, x, y) {
+        super().loadImage(imagePath);
+        this.x = x;
+        this.y = y;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,11 @@ class World {
     ];
     clouds = [
         new Cloud()
-    ]
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+    ];
+    
     canvas
     ctx;
     constructor(canvas) {
@@ -21,19 +25,27 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
-
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
-    
+        this.addToMap(this.character)
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
+        
+        /**
+         * draw() is called again and again
+         */
         let self = this;
         requestAnimationFrame(function() {
             self.draw();
         });
-    }   
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+    
+    addToMap(moveObject) {
+        this.ctx.drawImage(moveObject.img, moveObject.x, moveObject.y, moveObject.width, moveObject.height);
+    }
 }
