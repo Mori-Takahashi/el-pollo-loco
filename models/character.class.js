09a8f0abf53cfce3da327028d15d4895fa80a67f
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-28 10:30:01 +0200

    add Chicken animation (Walk)

--- models/character.class.js
+++ models/character.class.js
@@ -11,7 +11,6 @@ class Character extends movableObject{
         'img/2_character_pepe/2_walk/W-26.png'
     ];
 
-    currentImage = 0;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
