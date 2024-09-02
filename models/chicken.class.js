5f94b004b25ffd8f2c53296f480e0ab24d1087e3
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-09-02 14:04:18 +0200

    add small chickens

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends movableObject {
-    y = 360;
-    height = 60;
-    width = 50;
+    y = 330;
+    height = 80;
+    width = 70;
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
